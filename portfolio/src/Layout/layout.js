81285fcbfd8a105c9812a2cbba9a4c7f30e3f78a
Maximilian Schnitt <mx.schnitt@t-online.de>
2023-04-12 21:30:24 +0200

    deploying on GitHub Pages

--- portfolio/src/Layout/layout.js
+++ portfolio/src/Layout/layout.js
@@ -1,29 +1,22 @@
-import React from 'react';
-import { Outlet} from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-
-import classes from './layout.module.css'
-import Header from '../Header/header'
-import Footer from '../Footer/footer'
-
-
-
+import classes from "./layout.module.css";
+import Header from "../Header/header";
+import Footer from "../Footer/footer";
 
 const LayoutWebsite = () => {
-    return (
-        <div className={classes.layout__container}>
-        
-                <Header/>
-            
-            <main className={classes.main__body__container}>
-                <Outlet/>
-            </main>
-         
-            <Footer/>
-            
+  return (
+    <div className={classes.layout__container}>
+      <Header />
 
-        </div>
-    );
-}
+      <main className={classes.main__body__container}>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default LayoutWebsite;
