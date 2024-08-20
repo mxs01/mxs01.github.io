1ab8d7327ca879554178a30ab6b5ef0b93df3153
Maximilian Schnitt <91473170+mxs01@users.noreply.github.com>
2024-08-21 00:45:14 +0200

    responsive homepage

--- src/pages/Homepage/homepage.tsx
+++ src/pages/Homepage/homepage.tsx
@@ -1,34 +1,55 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography, useMediaQuery } from "@mui/material";
 import { NavBar } from "../../components/NavBar/NavBar";
 import classes from "./homepage.module.css";
 
 import profilePicture from "../../images/ProfilePicture.jpeg";
+import { Theme } from "@mui/material/styles";
 
 export const Homepage = (props: any) => {
+  const mobileQuery = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.between("xs", "sm")
+  );
   return (
     <Container
       maxWidth="xl"
       sx={{
         width: "80%",
-        height: "70vh",
-        marginBottom:"5rem"
+        height: {
+          xs: "30vh",
+          sm: "70vh",
+        },
+        marginBottom: {
+          xs:"0",
+          sm:"5rem"
+        },
       }}
     >
-      <NavBar />
+      <NavBar/>
       <Grid
         container
         sx={{
-          height: "80%",
+          height: {
+            xs: "20%",
+            xl: "80%",
+          },
         }}
       >
         <Grid
           item
-          xs={6}
+          sm={6}
+          xs={12}
           sx={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            marginTop: "6rem",
+            marginTop: {
+              xs: "2rem",
+              sm: "6rem",
+            },
+            order: {
+              xs: 2,
+              sm: 1,
+            },
           }}
         >
           <Box
@@ -51,7 +72,7 @@ export const Homepage = (props: any) => {
                 alignItems: "center",
                 width: "60%",
                 gap: "0.8rem",
-                marginBottom:"20px"
+                marginBottom: "20px",
               }}
             >
               <Typography variant="h6">React</Typography>
@@ -63,18 +84,36 @@ export const Homepage = (props: any) => {
 
         <Grid
           item
-          xs={6}
+          sm={6}
+          xs={12}
           sx={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            marginTop: "6rem",
+            marginTop: {
+              xs: "2rem",
+              sm: "6rem",
+            },
+            order: {
+              xs: 1,
+              sm: 2,
+            },
           }}
         >
           <Box
             sx={{
-              width: "350px",
-              height: "350px",
+              width: {
+                xs: "100px",
+                sm: "250",
+                lg: "350px",
+                xl: "450px",
+              },
+              height: {
+                xs: "100px",
+                sm: "250",
+                lg: "350px",
+                xl: "450px",
+              },
               borderRadius: "100%",
               overflow: "hidden",
               display: "flex",
@@ -82,7 +121,6 @@ export const Homepage = (props: any) => {
               alignItems: "center",
             }}
           >
-           
             <img
               id={classes.profilePicture}
               src={profilePicture}
@@ -94,3 +132,4 @@ export const Homepage = (props: any) => {
     </Container>
   );
 };
+
