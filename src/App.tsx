4912a2b96f417f83756d713788fdcef52d1de9d1
Maximilian Schnitt <91473170+mxs01@users.noreply.github.com>
2024-08-21 00:53:59 +0200

    Extracted navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import { Footer } from "./components/Footer/Footer";
 import { Contact } from "./pages/Contact/Contact";
 import { Homepage } from "./pages/Homepage/homepage";
 import { Work } from "./pages/Work/work";
+import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <Container maxWidth="xl" disableGutters sx={{
-
-    }}>
+    <Container maxWidth="xl" disableGutters sx={{}}>
+      <NavBar />
       <Homepage />
       <Work />
-      <Contact/>
+      <Contact />
       <Footer />
     </Container>
   );
